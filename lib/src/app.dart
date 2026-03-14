3ed680408515f8ac7170b2a9d2f4f39ef71af02f
BZR4 <BZR4@users.noreply.github.com>
2026-03-13 21:08:37 -0300

    Refactor Widgets

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_design_playground/src/pages/fun_with_flags.dart';
 import 'package:flutter_design_playground/src/pages/home_page.dart';
 
 // Stateless Widget
---
+++ lib/src/pages/widgets/base_flag_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BaseFlagWidget extends StatelessWidget {
+  const BaseFlagWidget({super.key, required this.flag});
+
+  final Widget flag;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 5 / 3,
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 2),
+        ),
+        child: flag,
+      ),
+    );
+  }
+}
